85fe8b040b763572410e4238b65b43dc569b5d5e
pratham-exe <prathamrao001@gmail.com>
2025-06-03 16:43:25 +0530

    malloc function

--- trash.c
+++ trash.c
@@ -56,3 +56,41 @@ static head* request_more_memory_from_kernel(size_t number_of_units)
 	add_block_to_free_block_list(new_block_from_memory);
 	return free_block;
 }
+
+void* trash_malloc(size_t size_of_malloc)
+{
+	size_t number_of_units;
+	head *temp, *prev;
+
+	number_of_units = ((size_of_malloc + sizeof(head) - 1) / sizeof(head)) + 1;
+	prev = free_block;
+
+	for (temp = prev->next_block;; prev = temp, temp = temp->next_block) {
+		if (temp->size_of_block >= number_of_units) {
+			if (temp->size_of_block == number_of_units) {
+				prev->next_block = temp->next_block;
+			} else {
+				temp->size_of_block -= number_of_units;
+				temp += temp->size_of_block;
+				temp->size_of_block = number_of_units;
+			}
+
+			free_block = prev;
+			if (used_block == NULL) {
+				temp->next_block = temp;
+				used_block = temp;
+			} else {
+				temp->next_block = used_block->next_block;
+				used_block->next_block = temp;
+			}
+
+			return (void*)temp + 1;
+		}
+		if (temp == free_block) {
+			temp = request_more_memory_from_kernel(number_of_units);
+			if (temp == NULL) {
+				return NULL;
+			}
+		}
+	}
+}
