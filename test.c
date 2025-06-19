718807f1258e8e3ea42a01f4444dba182dc50e6b
pratham-exe <prathamrao001@gmail.com>
2025-06-19 17:39:12 +0530

    works with arrays

--- test.c
+++ test.c
@@ -20,22 +20,31 @@ int main()
 	trash_init_and_find_stack_bottom();
 
 	int* a = (int*)trash_malloc(sizeof(int));
-	int* b = (int*)trash_malloc(sizeof(int));
+	int* arr = (int*)trash_malloc(5 * sizeof(int));
+	for (int i = 0; i < 5; ++i) {
+		arr[i] = i * 10;
+	}
 
 	*a = 42;
-	*b = 69;
 
 	trash_mark_block_live();
 	dump_liveness("a", a);
-	dump_liveness("b", b);
+	dump_liveness("arr", arr);
 	trash_collection();
 
 	a = NULL;
-	printf("\nAfter GC\n\n");
+	printf("\na is NULL\n\n");
 
 	trash_mark_block_live();
 	dump_liveness("a", a);
-	dump_liveness("b", b);
+	dump_liveness("arr", arr);
+	trash_collection();
+
+	arr = NULL;
+	printf("\narr is NULL\n\n");
+
+	dump_liveness("a", a);
+	dump_liveness("arr", arr);
 	trash_collection();
 
 	return 0;
